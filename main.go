45ea8237996b552dcd75937f16b1b2cbea222b21
claredavies <clare.davies@abo.fi>
2023-09-20 15:13:07 +0100

    Able to access price with specific key (need to make it general WIP)

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-    "encoding/json"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-    "emrPricingAPI/constants"
+	"emrPricingAPI/constants"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -22,40 +23,47 @@ func main() {
 	// Create AWS credentials
 	creds := credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, awsSessionToken)
 
-    // Create a new AWS session with custom configuration
-    sess, err := session.NewSession(&aws.Config{
-        Region:      aws.String(awsRegion),
-        Credentials: creds,
-    })
-    if err != nil {
-        log.Fatalf("Error creating AWS session: %v", err)
-    }
+	// Create a new AWS session with custom configuration
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(awsRegion),
+		Credentials: creds,
+	})
+	if err != nil {
+		log.Fatalf("Error creating AWS session: %v", err)
+	}
 
-    // Create a Pricing service client using the custom session
-    pricingSvc := pricing.New(sess)
+	// Create a Pricing service client using the custom session
+	pricingSvc := pricing.New(sess)
 
-    // Make a basic request to the Pricing API
-    input := &pricing.GetProductsInput{
-        ServiceCode:   aws.String("AmazonEC2"),
-        FormatVersion: aws.String("aws_v1"),
-    }
-    result, err := pricingSvc.GetProducts(input)
-    if err != nil {
-        log.Fatalf("Error fetching AWS Pricing API data: %v", err)
-    }
+	// Make a basic request to the Pricing API
+	input := &pricing.GetProductsInput{
+		ServiceCode:   aws.String("AmazonEC2"),
+		FormatVersion: aws.String("aws_v1"),
+	}
+	result, err := pricingSvc.GetProducts(input)
+	if err != nil {
+		log.Fatalf("Error fetching AWS Pricing API data: %v", err)
+	}
 
-    // Save the response as JSON
-    jsonData, err := json.MarshalIndent(result, "", "    ")
-    if err != nil {
-        log.Fatalf("Error encoding JSON: %v", err)
-    }
+	// Save the response as JSON
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Fatalf("Error encoding JSON: %v", err)
+	}
 
-    // Write the JSON data to a file
-    outputFile := "pricing_response.json"
-    err = os.WriteFile(outputFile, jsonData, 0644)
-    if err != nil {
-        log.Fatalf("Error writing JSON to file: %v", err)
-    }
+	// Write the JSON data to a file
+	outputFile := "pricing_response.json"
+	err = os.WriteFile(outputFile, jsonData, 0644)
+	if err != nil {
+		log.Fatalf("Error writing JSON to file: %v", err)
+	}
 
-    fmt.Printf("Pricing data saved to %s\n", outputFile)
+	// Extract the desired fields from the JSON response
+	for _, priceListItem := range result.PriceList {
+		terms := priceListItem["terms"].(map[string]interface{})["OnDemand"].(map[string]interface{})
+        priceDimensions := terms["2223B6PCG6QAUYY6.JRTCKXETXF"].(map[string]interface{})["priceDimensions"].(map[string]interface{})
+        pricePerUnit := priceDimensions["2223B6PCG6QAUYY6.JRTCKXETXF.6YS6EN2CT7"].(map[string]interface{})["pricePerUnit"].(map[string]interface{})["USD"].(string)
+
+		fmt.Printf("Price Per Unit: %s\n", pricePerUnit)
+	}
 }
