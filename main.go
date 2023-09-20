25f6fa0169c8119a4ab51cba57854290528909e6
claredavies <clare.davies@abo.fi>
2023-09-20 12:06:40 +0100

    Able to speak to aws sdk for pricing

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "log"
-
+    "encoding/json"
+	"fmt"
+	"log"
+	"os"
     "emrPricingAPI/constants"
-
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/credentials"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/pricing"
 )
 
 func main() {
-    // Replace these values with your own AWS credentials and region
-    awsAccessKeyID := constants.AwsAccessKeyID
-    awsSecretAccessKey := constants.AwsSecretAccessKey
-    awsSessionToken := constants.AwsSessionToken
-    awsRegion := "us-east-1" // Replace with your desired AWS region
+	// Replace these values with your own AWS credentials and region
+	awsAccessKeyID := constants.AwsAccessKeyID
+	awsSecretAccessKey := constants.AwsSecretAccessKey
+	awsSessionToken := constants.AwsSessionToken
+	awsRegion := "us-east-1" // Replace with your desired AWS region
 
-    // Create AWS credentials
-    creds := credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, awsSessionToken)
+	// Create AWS credentials
+	creds := credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, awsSessionToken)
 
     // Create a new AWS session with custom configuration
     sess, err := session.NewSession(&aws.Config{
@@ -31,18 +31,31 @@ func main() {
         log.Fatalf("Error creating AWS session: %v", err)
     }
 
-    // Create an S3 service client using the custom session
-    svc := s3.New(sess)
+    // Create a Pricing service client using the custom session
+    pricingSvc := pricing.New(sess)
 
-    // List S3 buckets
-    result, err := svc.ListBuckets(nil)
+    // Make a basic request to the Pricing API
+    input := &pricing.GetProductsInput{
+        ServiceCode:   aws.String("AmazonEC2"),
+        FormatVersion: aws.String("aws_v1"),
+    }
+    result, err := pricingSvc.GetProducts(input)
     if err != nil {
-        log.Fatalf("Error listing S3 buckets: %v", err)
+        log.Fatalf("Error fetching AWS Pricing API data: %v", err)
     }
 
-    // Print the names of the S3 buckets
-    fmt.Println("S3 Buckets:")
-    for _, b := range result.Buckets {
-        fmt.Printf("* %s created on %s\n", aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+    // Save the response as JSON
+    jsonData, err := json.MarshalIndent(result, "", "    ")
+    if err != nil {
+        log.Fatalf("Error encoding JSON: %v", err)
     }
+
+    // Write the JSON data to a file
+    outputFile := "pricing_response.json"
+    err = os.WriteFile(outputFile, jsonData, 0644)
+    if err != nil {
+        log.Fatalf("Error writing JSON to file: %v", err)
+    }
+
+    fmt.Printf("Pricing data saved to %s\n", outputFile)
 }
