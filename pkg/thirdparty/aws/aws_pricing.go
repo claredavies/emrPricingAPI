bd76e1dfbfab6f9f046874a3996734d773f57af2
claredavies <clare.davies@abo.fi>
2023-09-21 11:35:58 +0100

    Adding in Price struct

--- pkg/thirdparty/aws/aws_pricing.go
+++ pkg/thirdparty/aws/aws_pricing.go
@@ -3,11 +3,23 @@ package aws
 import (
     "encoding/json"
     "fmt"
+    "strconv"
     "os"
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/pricing"
 )
+
+type Price struct {
+    ID              string  `json:"ID"`
+    ServiceType     string  `json:"ServiceType"`
+    InstanceType    string  `json:"InstanceType"`
+    Market          string  `json:"Market"`
+    Unit            string  `json:"Unit"`
+    PricePerUnit    float64 `json:"PricePerUnit"`
+    PriceDescription string  `json:"PriceDescription"`
+    UpdatedAt       string  `json:"UpdatedAt"`
+}
 
 func FetchAndSavePricingData() error {
 	sess, err := session.NewSession(&aws.Config{
@@ -33,39 +45,69 @@ func FetchAndSavePricingData() error {
 	saveResponseToJson(result)
 
 	// Extract the price per unit cost for each instance type
-	extractPricingInformation(result)
+	prices := extractPricingInformation(result)
+	fmt.Println(prices)
 
 	return nil
 }
 
-func extractPricingInformation(result *pricing.GetProductsOutput) {
-	for _, priceListItem := range result.PriceList {
-		// Iterate over both "OnDemand" and "Reserved" terms
-		for _, reservationType := range []string{"OnDemand", "Reserved"} {
-			terms := priceListItem["terms"].(map[string]interface{})[reservationType]
-			if terms == nil {
-				continue // Skip if the reservation type is not present for this product
-			}
+func extractPricingInformation(result *pricing.GetProductsOutput) []Price {
+    var prices []Price
 
-			// Iterate over the terms and extract the pricing information
-			for _, product := range terms.(map[string]interface{}) {
-				priceDimensions := product.(map[string]interface{})["priceDimensions"].(map[string]interface{})
-				firstNestedElement := map[string]interface{}{} // Initialize an empty map for the first nested element
+    for _, priceListItem := range result.PriceList {
+        // Extract product attributes
+        productAttributes, _ := priceListItem["product"].(map[string]interface{})["attributes"].(map[string]interface{})
+        instanceType, _ := extractInstanceType(productAttributes)
 
-				// Iterate over the price dimensions to access the first nested element
-				for _, dimension := range priceDimensions {
-					firstNestedElement = dimension.(map[string]interface{})
-					break // Break after accessing the first nested element
-				}
+        serviceCode := productAttributes["servicecode"].(string)
+//         regionCode := productAttributes["regionCode"].(string)
 
-				// Extract the price per unit cost and instance type from the first nested element
-				pricePerUnit := firstNestedElement["pricePerUnit"].(map[string]interface{})["USD"].(string)
-				instanceType := firstNestedElement["description"].(string)
+        // Iterate over both "OnDemand" and "Reserved" terms
+        for _, reservationType := range []string{"OnDemand", "Reserved"} {
+            terms := priceListItem["terms"].(map[string]interface{})[reservationType]
+            if terms == nil {
+                continue // Skip if the reservation type is not present for this product
+            }
 
-				fmt.Printf("Reservation Type: %s, Instance Type: %s, Price Per Unit: %s\n", reservationType, instanceType, pricePerUnit)
-			}
-		}
-	}
+            // Iterate over the terms and extract the pricing information
+            for _, product := range terms.(map[string]interface{}) {
+                priceDimensions := product.(map[string]interface{})["priceDimensions"].(map[string]interface{})
+                for _, dimension := range priceDimensions {
+                    pricePerUnit, _ := dimension.(map[string]interface{})["pricePerUnit"].(map[string]interface{})["USD"].(string)
+                    pricePerUnitFloat, _ := strconv.ParseFloat(pricePerUnit, 64)
+                    priceDescription := dimension.(map[string]interface{})["description"].(string)
+
+                    price := Price{
+                        ID:              "", // Set the ID as needed
+                        ServiceType:     serviceCode,
+                        InstanceType:    instanceType,
+                        Market:          reservationType,
+                        Unit:            "USD",
+                        PricePerUnit:    pricePerUnitFloat,
+                        PriceDescription: priceDescription,
+                        UpdatedAt:       "", // Set the update timestamp as needed
+                    }
+
+                    prices = append(prices, price)
+                }
+            }
+        }
+    }
+
+    return prices
+}
+
+func extractInstanceType(productAttributes map[string]interface{}) (string, bool) {
+    fieldNames := []string{"instanceType", "compute", "usagetype"}
+
+    for _, fieldName := range fieldNames {
+        instanceType, ok := productAttributes[fieldName].(string)
+        if ok {
+            return instanceType, true
+        }
+    }
+
+    return "", false
 }
 
 func saveResponseToJson(result *pricing.GetProductsOutput) error {
@@ -93,11 +135,11 @@ func defineFilter() *pricing.GetProductsInput {
     				Field: aws.String("location"),
     				Value: aws.String("US West (Oregon)"),
     			},
-    			{
-    				Type:  aws.String("TERM_MATCH"),
-    				Field: aws.String("instanceType"),
-    				Value: aws.String("C6g.12xlarge"), // Add the instance type filter here
-    			},
+//     			{
+//     				Type:  aws.String("TERM_MATCH"),
+//     				Field: aws.String("instanceType"),
+//     				Value: aws.String("C6g.12xlarge"), // Add the instance type filter here
+//     			},
     		},
     }
     return input
