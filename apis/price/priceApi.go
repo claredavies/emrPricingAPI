192d432e0e46ecd151c193137c78444573eae954
claredavies <clare.davies@abo.fi>
2023-09-25 11:45:26 +0100

    Updated fetchJsonUnstructured and fetchJsonUnstructuredFilter to take in query parameters

--- apis/price/priceApi.go
+++ apis/price/priceApi.go
@@ -13,44 +13,28 @@ import (
 var prices = []models.Price{
 }
 
-//input region and service
-func LoadPrices() {
-    newPrices, _ := aws.FetchPricingData("us-east-1", "ElasticMapReduce")
+func LoadPrices(region string, service string) {
+    newPrices, _ := aws.FetchPricingData(region, service)
     prices = append(prices, newPrices...)
 }
 
-func getPrice(c echo.Context) error {
+func getOnePrice(c echo.Context) ([]models.Price, error) {
     region:=c.QueryParam("region")
     serviceCode:=c.QueryParam("serviceCode")
     location:=c.QueryParam("location")
     instanceType:=c.QueryParam("instanceType")
 
     if region == ""|| serviceCode == ""|| location == ""|| instanceType == ""{
-        return c.JSON(http.StatusBadRequest, echo.Map{"message": constants.ErrMsgQueryGetPrice})
+        return nil, c.JSON(http.StatusBadRequest, echo.Map{"message": constants.ErrMsgQueryGetPrice})
     }
 
-    onePrice, _ := aws.FetchPricingDataFilter(region, serviceCode, location, instanceType)
+    onePrice, err := aws.FetchPricingDataFilter(region, serviceCode, location, instanceType)
+    return onePrice, err
+}
+
+func getPrice(c echo.Context) error {
+    onePrice, _ := getOnePrice(c)
     err := c.JSON(http.StatusOK, onePrice)
-    if err != nil {
-        return err
-    }
-
-    return nil
-}
-
-func fetchJsonUnstructured(c echo.Context) error {
-    jsonResult, _ := aws.FetchPricingDataJson("us-east-1", "ElasticMapReduce")
-    err := c.JSON(http.StatusOK, jsonResult)
-    if err != nil {
-        return err
-    }
-
-    return nil
-}
-
-func fetchJsonUnstructuredFilter(c echo.Context) error {
-    jsonResult, _ := aws.FetchPricingDataJsonFilter("us-east-1", "ElasticMapReduce", "US West (Oregon)", "C6g.12xlarge")
-    err := c.JSON(http.StatusOK, jsonResult)
     if err != nil {
         return err
     }
@@ -67,9 +51,45 @@ func getPrices(c echo.Context) error {
     return nil
 }
 
+func fetchJsonUnstructured(c echo.Context) error {
+    region :=c.QueryParam("region")
+    serviceCode :=c.QueryParam("serviceCode")
+
+    if region == ""|| serviceCode == "" {
+            return c.JSON(http.StatusBadRequest, echo.Map{"message": constants.ErrMsgQueryGetPrices})
+    }
+
+    jsonResult, _ := aws.FetchPricingDataJson(region, serviceCode)
+    err := c.JSON(http.StatusOK, jsonResult)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func fetchJsonUnstructuredFilter(c echo.Context) error {
+    region:=c.QueryParam("region")
+    serviceCode:=c.QueryParam("serviceCode")
+    location:=c.QueryParam("location")
+    instanceType:=c.QueryParam("instanceType")
+
+    if region == ""|| serviceCode == ""|| location == ""|| instanceType == ""{
+        return c.JSON(http.StatusBadRequest, echo.Map{"message": constants.ErrMsgQueryGetPrice})
+    }
+
+    jsonResult, _ := aws.FetchPricingDataJsonFilter(region, serviceCode, location, instanceType)
+    err := c.JSON(http.StatusOK, jsonResult)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
 func SetupRoutes(e *echo.Echo) {
     e.GET("/prices", getPrices)
-    e.GET("/getPrice", getPrice)
+    e.GET("/price", getPrice)
     e.GET("/unstructuredJson", fetchJsonUnstructured)
     e.GET("/unstructuredJsonFilter", fetchJsonUnstructuredFilter)
 }
--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 func main() {
     e := echo.New()
     price.SetupRoutes(e)
-    price.LoadPrices()
+    price.LoadPrices("us-east-1", "ElasticMapReduce")
     e.Start("localhost:8080")
 }
